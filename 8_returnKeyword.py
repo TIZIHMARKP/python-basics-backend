d7623e3dd2144832268e436508b79989ca505da8
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-03-21 11:41:44 +0100

    Learned about the basics of the return keyword in python

---
+++ 8_returnKeyword.py
@@ -0,0 +1,20 @@
+
+# ====================  Return Keyword In Python =====================
+# The return statement gives us a feedback of what is being executed in a function
+
+def myFunction():
+    return 5+4
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def add_numbers1(num1, num2):
+    return num1 + num2
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(myFunction())
+print(add_numbers(20, 20))
+print(add_numbers1(num1, num2))
+
