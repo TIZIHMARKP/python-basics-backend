ab13a9843ff518091c6de0f5029ca407acb234b3
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-03-22 22:09:24 +0100

    Learned basics of dictionary in python

---
+++ 11_dict.py
@@ -0,0 +1,22 @@
+
+# ==================== Dictionaries In Python =====================
+# Dictionaries are used to store data values in the key values pair. Dictionaries are changeble but they don't allow duplicate key
+# We can also add a list into dictionaries
+
+my_dict = {
+    'name': 'Prince',
+    'name2': 'Prince',
+    'nationality': 'African',
+    'Qualification': 'Degree',
+    'age': 120,
+    'is_tall': True,
+    'friends': ['Peter', 'Paul', 'Solomon', 'Joseph']
+}
+
+x = my_dict['name']
+
+print(my_dict)
+print(type(my_dict))
+print(my_dict['is_tall'])
+print(my_dict['friends'])
+print(x)
