43a0a02f7dec18ef50130e75d0a9cef91e6f3d1a
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-03-22 03:00:25 +0100

    Learned the ifStatement together with a basic program to determine if a number is even

---
+++ 10_evenNumber.py
@@ -0,0 +1,10 @@
+
+# ==================== Python program to know if a Number is Even =====================
+# 
+
+num = int(input('Enter a number: '))
+
+if num%2 == 0:
+    print('Even number')
+else:
+    print('Odd number')
