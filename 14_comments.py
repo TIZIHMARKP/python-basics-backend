7bb06badfe487b19e421894299491c10d81fd46b
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-03-22 23:11:27 +0100

    Learned about comments in python

---
+++ 14_comments.py
@@ -0,0 +1,9 @@
+
+# ==================== Comments In Python =====================
+#  Comments is been used to prevent a particular code or block of code from running in your code. Comments is used for making our code readable
+
+
+#print('hello) # This is a comment
+print(1)
+
+
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 
 # ==================== Comments In Python =====================
-#  Comments is been used to prevent a particular code or block of code from running in your code
+# 
+
