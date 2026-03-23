2ee7f2cbbb13580a79457e6189d097fa04f5cbd5
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-03-23 10:01:22 +0100

    Learned the basics of modules in python

---
+++ 22_modules.py
@@ -0,0 +1,7 @@
+
+# ====================  Modules In python =====================
+#  Modules allows you to get everything present in another file and use them in your files or projects
+
+import new1
+
+new1.sayHi()
