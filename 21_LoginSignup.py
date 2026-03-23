187e1cff834ada56da39708a68d9c10ed0b8ad05
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-03-23 09:55:18 +0100

    Built a simple loginSignup system with python

---
+++ 21_LoginSignup.py
@@ -0,0 +1,18 @@
+
+# ====================  Building Simple Login and Signup system with python =====================
+#  
+
+print('Create account now')
+username = input('Enter username: ')
+password = input('Enter Password: ')
+
+print('Your account has been created successfully')
+print('Login now')
+
+username2 = input('Enter username: ')
+password2 = input('Enter password: ')
+
+if username == username2 and password == password2:
+    print('Logged in successfully')
+else:
+    print('Invalid credentials')
