d05680bdc639966a23c3d589adb8b0ee6b8f32a9
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-04-30 12:52:00 +0100

    Writen the solution to question 9 which counts number of vowels in a sentence and prints the length of sentence if sentence is greater than 20

--- app.py
+++ app.py
@@ -27,6 +27,45 @@ Converts the sentence to title case and prints it.
 
 Use a combination of loops, conditionals, and string methods.
 """
+# === Solution 1 ===
+"""
+- Arithmetic operators are used to perform mathematical calculations. They take a number as input and then return a number as output. These operators in python are addition(+), subtraction(-), multiplication(*), division(/), modulus(%), and exponent(**). 
+- Logical operators are operators which are used to combine boolean values and also returns a boolean value as the result. They include "and"(both must be True), "or"(at least one must be True) and "not"(reverses the truth value)
+- Comparison operators are operators which are always used to compare two values. They always return a boolean result as output. Examples include: ==(equal to), !=(not equal to), >(greater than), <(less than), >=(greater than or equal to) and <=(less than or equal to) 
+"""
+
+# === Solution 2 ===
+"""
+Operator precedence is the rule in which each operator in an expression gets executed first. For example, multiplication is performed first before addition (2 + 3 * 5 equals 17, and not 25). It follows the rules of BODMAS in mathematics
+It is important because of the following reasons
+- It prevents logical errors, meaning the code might run but still give a wrong answer
+- It further removes ambiguity. Without precedence rules, one expression could mean different things and the python interpreter might have to guess the correct expression
+- Precedence rules makes code predictable. Since everyone who learns python learns the same rules, any programmer can read your code and know what it is doing 
+"""
+
+# === Solution 3 ===
+"""
+- The "is" is an identity operator while "==" is a comparison operator
+- The "is" operator checks if two objects have same identity equality while "==" operator checks if two objects contain the same equality
+"""
+# === Solution 4 ===
+"""
+The function of the modulus operator is the get the remainder of a division operation. 
+It can be applied to check if a number is even or odd. If number % 2 gives a remainder of 0, then it is an even number else odd number 
+"""
+
+# === Solution 5 ===
+"""
+f-stings is a way to embed variables and expressions directly inside strings. The f stands for formats which makes formatting easier and more readable in a code. Below is an example of how f-strings are used
+name = 'Tizih'
+school = 'Computer Engineering'
+message = f"my name is {name} and I school at {school}"
+print(message)
+It is used by putting an f before the opening quote and curly braces {} are used to insert variables or expressions in between the opening and closing quote
+"""
+# === Solution 6 ===
+
+
 
 # === Solution 7 ===
 
