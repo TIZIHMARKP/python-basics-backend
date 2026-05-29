94e731b3815c6136131f49f670fd65562a3cdae0
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-05-29 22:03:42 +0100

    Writing a python program to display all prime numbers within an interval

--- basicPrograms/2_primeNumber.py
+++ basicPrograms/2_primeNumber.py
@@ -4,21 +4,25 @@
 # except for 1 and itself. For example, 2, 3, 5, 7, 11, and 13 are prime numbers because they
 # cannot be divided by any other positive integer except for 1 and their own value.
 
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-# define a flag variable
-flag = False
-if num == 1:
-    print(f"{num} is not a prime number ")
-elif num > 1:
-    # check for factors
-    for i in range(2, num):
-        if (num % i) == 0:
-            flag = True     # If factor is found, set flag to True
-            break          # breakx out of the loop
+# # define a flag variable
+# flag = False
+# if num == 1:
+#     print(f"{num} is not a prime number ")
+# elif num > 1:
+#     # check for factors
+#     for i in range(2, num):
+#         if (num % i) == 0:
+#             flag = True     # If factor is found, set flag to True
+#             break          # breakx out of the loop
 
-if flag:                    # checkx if flag is True
-    print(f"{num} is not a prime number")
-else:
-    print(f"{num} is a prime number")
+# if flag:                    # checkx if flag is True
+#     print(f"{num} is not a prime number")
+# else:
+#     print(f"{num} is a prime number")
+
+
+# Program to display all the prime numbers within an interval
+
 
