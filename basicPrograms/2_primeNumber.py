7d852f51faa8f9f56f047f759edcf16ce6b37590
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-05-29 22:22:17 +0100

    Added comments to the code

--- basicPrograms/2_primeNumber.py
+++ basicPrograms/2_primeNumber.py
@@ -31,6 +31,7 @@ upper = int(input("Enter the upper limit number:  "))
 print(f"Prime numbers between {lower} and {upper} are: ")
 
 for num in range(lower, upper + 1):
+    # All prime numbers are greater than 1
     if num > 1:
         for i in range(2, num):
             if (num % i) == 0:
