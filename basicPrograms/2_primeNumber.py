116f0d59001d6a6a4cc9fb79a734a282d578567c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-05-29 22:21:29 +0100

    Successfully written program to get prime numbers within a range

--- basicPrograms/2_primeNumber.py
+++ basicPrograms/2_primeNumber.py
@@ -25,4 +25,17 @@
 
 # Program to display all the prime numbers within an interval
 
+lower = 1
+upper = int(input("Enter the upper limit number:  "))
+
+print(f"Prime numbers between {lower} and {upper} are: ")
+
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
+
 
