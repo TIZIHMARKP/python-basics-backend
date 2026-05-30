0905642b4792bcb14a466a6a4dc5f7ef78ca4848
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-05-30 22:06:01 +0100

    Successfully written program to calculate fibonacy sequence

--- basicPrograms/5_fibonacciSeq.py
+++ basicPrograms/5_fibonacciSeq.py
@@ -3,3 +3,20 @@
 # F(0) = 0 𝐹(1) = 1 𝐹(𝑛) = 𝐹(𝑛−1)+𝐹(𝑛−2)𝑓𝑜𝑟𝑛 > 1
 
 
+nterms = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+if nterms <= 0:
+    print("Please enter a positive integer")    
+elif nterms == 1:
+    print(f"Fibonacci sequence upto {nterms} is: ")
+    print(n1)
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        
+        count += 1
