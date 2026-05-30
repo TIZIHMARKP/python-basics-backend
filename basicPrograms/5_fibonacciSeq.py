3bdb86270cf1659328e0b41f8165b027c546fd49
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-05-30 22:08:13 +0100

    Optimized the code for fibonacy sequence

--- basicPrograms/5_fibonacciSeq.py
+++ basicPrograms/5_fibonacciSeq.py
@@ -4,13 +4,20 @@
 
 
 nterms = int(input("How many terms? "))
+# first two terms
 n1, n2 = 0, 1
 count = 0
+
+# checkx if the number of terms is valid
 if nterms <= 0:
-    print("Please enter a positive integer")    
+    print("Please enter a positive integer")  
+
+# If there is only 1 term, we return n1 = 0  
 elif nterms == 1:
     print(f"Fibonacci sequence upto {nterms} is: ")
     print(n1)
+
+# generate Fibonacci sequence
 else:
     print("Fibonacci sequence:")
     while count < nterms:
@@ -18,5 +25,5 @@ else:
         nth = n1 + n2
         n1 = n2
         n2 = nth
-        
+
         count += 1
