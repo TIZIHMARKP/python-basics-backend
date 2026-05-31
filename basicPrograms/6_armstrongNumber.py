16364c5b80abbee921fd43b88df21aaac308acfe
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-05-31 16:56:44 +0100

    I have explained what an Armstrong number is in the comments

---
+++ basicPrograms/6_armstrongNumber.py
@@ -0,0 +1,21 @@
+# Armstrong Number
+
+# It is a number that is equal to the sum of its own digits, each raised to a power equal to the
+# number of digits in the number.
+
+#For example, let's consider the number 153:
+
+#   It has three digits (1, 5, and 3).
+#   If we calculate 1^3 + 5^3 + 3^3, we get 1 + 125 + 27, which is equal to 153
+
+# So, 153 is an Armstrong number because it equals the sum of its digits raised to the power
+# of the number of digits in the number.
+
+# Another example is 9474
+
+#   It has four digits (9, 4, 7, and 4)
+#   If we calculate 9^4 + 4^4 + 7^4 + 4^4, we get 6561 + 256 + 2401 + 256, which is also 
+# equal to 9474
+
+# Therefore, 9474 is an Armstrong number as well.
+
