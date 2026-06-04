022ebe677375e62ce934d26932a58f51cf83befd
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-04 22:38:51 +0100

    Explained what LCM is and how it is calculated

---
+++ basicPrograms/9_lcm.py
@@ -0,0 +1,13 @@
+# LCM, or Least Common Multiple, is the smallest multiple that is exactly divisible by two or
+# more numbers.
+
+# Formula:
+
+# For two numbers a and b, the LCM can be found using the formula:
+
+#       LCM(a, b) = |a . b| / GCD(a, b)
+
+# For more than two numbers, we can find the LCM step by step, taking the LCM of pairs of
+# numbers at a time until we reach the last pair.
+
+# Python program to find the L.C.M of two input numbers
