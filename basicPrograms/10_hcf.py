0b6cfe5d8edfcc73f6d57ff6f297f43d3e83a231
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-04 23:03:12 +0100

    Explained what H.C.F means

---
+++ basicPrograms/10_hcf.py
@@ -0,0 +1,13 @@
+# Highest Common Factor(HCF):
+
+# HCF, or Highest Common Factor, is the largest positive integer that divides two or more
+# numbers without leaving a remainder
+# 
+# For two numbers a and b, the HCF can be found using the formula:
+
+#      HCF(a, b) = GCD(a, b)
+
+# For more than two numbers, you can find the HCF by taking the GCD of pairs of numbers at
+# a time until you reach the last pair.
+
+# Python program to find the H.C.F of two input numbers
