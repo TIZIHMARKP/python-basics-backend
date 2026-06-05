e3f76d6bff427f55be9a95e8be8338aa594efdb9
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-05 22:12:08 +0100

    Explained how to convert a decimal number to Binary, Octal, and Hex

---
+++ basicPrograms/11_dec2BinOctHex.py
@@ -0,0 +1,18 @@
+# Write a Python Program to Convert Decimal to Binary, Octal and Hexadecimal.
+
+# Converting a decimal number to binary, octal, and hexadecimal involves dividing the
+# decimal number by the base repeatedly and noting the remainders at each step. For e.g:
+
+# Binary = base 2
+# Octal = base 8
+# hexadecimal = base 16
+
+
+
+
+
+
+
+
+
+
