aee8e8b57afcf74c72fee395898d02d6cb22bf35
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-07 00:28:57 +0100

    Added self explanatory comments to the code above

--- basicPrograms/13_simpleCalculator.py
+++ basicPrograms/13_simpleCalculator.py
@@ -42,8 +42,8 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
 
-       
-
+        # check if user wants another calculation
+        # break the while loop if answer is no
         next_calculation = input("Let us do next calculation? (yes/no)")
         if next_calculation == "no":
             break
