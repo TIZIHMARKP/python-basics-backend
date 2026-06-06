c358237b01e3913be1ee154f343b9de935825c46
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-06 23:59:38 +0100

    Added comment to check if user wants another calculation so it loops back again

--- basicPrograms/13_simpleCalculator.py
+++ basicPrograms/13_simpleCalculator.py
@@ -43,4 +43,5 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
 
-        
+        # check if user wants another calculation
+        # break the while loop if answer is no
