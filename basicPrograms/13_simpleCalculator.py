5865dc33cfb95f66f800973f62d7e9b92a5a0493
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-06 23:54:51 +0100

    Added some simple comments to the code function

--- basicPrograms/13_simpleCalculator.py
+++ basicPrograms/13_simpleCalculator.py
@@ -4,26 +4,27 @@
 # This function adds two numbers
 def add(x, y):
     return x + y
-
 # This function subtracts two numbers
 def subtract(x, y):
     return x - y
-
+# This function multiplies two numbers
 def multiply(x, y):
     return x * y
-
+# This function divides two numbers
 def divide(x, y):
     return x / y
+
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-
 while True:
+    # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
-
+    
     if choice in ('1', '2', '3', '4'):
         try:
             num1 = float(input("Enter first number: "))
