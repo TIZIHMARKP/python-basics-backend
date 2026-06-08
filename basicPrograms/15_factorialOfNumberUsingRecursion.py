9e504eaeba60b692e80ff5e59aacab9199ceb94f
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-08 22:18:01 +0100

    Explained and broken down how to calculate factorial using recursion

---
+++ basicPrograms/15_factorialOfNumberUsingRecursion.py
@@ -0,0 +1,12 @@
+# Python Program to Find Factorial of Number Using Recursion.
+
+# he factorial of a non-negative integer ( n ) is the product of all positive integers less than or
+# equal to ( n ). It is denoted by ( n! ) and is defined as:
+
+#  n! = 𝑛×(𝑛−1)×(𝑛−2)×…×3×2×1
+# For example
+
+# 5! = 5 × 4 × 3 × 2 × 1 = 120
+# 0! is defined to be 1
+
+
