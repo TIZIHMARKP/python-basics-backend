1409633b346bc382b5e3734af1a15134952fa468
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-08 21:47:41 +0100

    Explained the formula used to compute fibonacci using recursion

---
+++ basicPrograms/14_fibonacciSequenceUsingRecursion.py
@@ -0,0 +1,14 @@
+# Writing a Python Program to Display Fibonacci Sequence Using Recursion.
+
+# Fibonacci Sequence: 
+
+# The Fibonacci sequence is a series of numbers in which each number is the sum of the two
+# preceding ones, usually starting with 0 and 1. In mathematical terms, it is defined by the
+# recurrence relation ( F(n) = F(n-1) + F(n-2) ), with initial conditions ( F(0) = 0 ) and ( F(1) = 1
+# ). The sequence begins: 0, 1, 1, 2, 3, 5, 8, 13, 21, and so on. The Fibonacci sequence has
+# widespread applications in mathematics, computer science, nature, and art.
+
+
+
+
+
