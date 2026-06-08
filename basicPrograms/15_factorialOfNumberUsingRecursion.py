8b1274f861f76d5c62f6db81ae6fa707b8d7e971
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-08 22:22:54 +0100

    Written a python program to compute the factorial of a number using recursion

--- basicPrograms/15_factorialOfNumberUsingRecursion.py
+++ basicPrograms/15_factorialOfNumberUsingRecursion.py
@@ -9,4 +9,20 @@
 # 5! = 5 × 4 × 3 × 2 × 1 = 120
 # 0! is defined to be 1
 
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n * recur_factorial(n-1)
+    
+num = int(input("Enter the number: "))
 
+if num < 0:
+    print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+    print("The factorial of zero is 1")
+else:
+    print("The factorial of ", num, "is: ", recur_factorial(num))
+
+
+    
