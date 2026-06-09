46eff7a3339516c985f3c70711d4b471497cf304
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-09 22:48:18 +0100

    Successfully written a program in python to calcuate Body Mass Index

--- basicPrograms/16_bodyMassIndex.py
+++ basicPrograms/16_bodyMassIndex.py
@@ -13,8 +13,26 @@
 # to assess potential health risks associated with weight.
 
 
+def bodyMassIndex(height, weight):
+    return round((weight / height**2), 2)
+
+h = float(input("Enter your height in meters: "))
+w = float(input("Enter your weight in kg: "))
+
+print("Welcome to the BMI calculator.")
+
+bmi = bodyMassIndex(h, w)
+print("Your BMI is: ", bmi)
 
 
+if bmi <= 18.5:
+    print("You are underweight. ")
+elif 18.5 < bmi <= 24.9:
+    print("Your weight is normal")
+elif 25 < bmi <= 29.29:
+    print("You are overweight")
+else:
+    print("You are obese.")
 
 
 
