6030faecbb268601e5a261c6623a9bfcdac3f76f
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-09 22:35:05 +0100

    Explained what body Mass Index is by giving it defination

---
+++ basicPrograms/16_bodyMassIndex.py
@@ -0,0 +1,15 @@
+# Python Program to calculate your Body Mass Index
+
+# Body Mass Index (BMI) is a measure of body fat based on an individual's weight and
+# height. It is commonly used as a screening tool to categorize individuals into different weight
+# status categories, such as underweight, normal weight, overweight, and obesity.
+
+
+
+
+
+
+
+
+
+
