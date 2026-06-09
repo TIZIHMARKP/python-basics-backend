e3ebef6d4ecb79ff8b86c33de533619769891ee3
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-09 22:38:37 +0100

    Listed the formula used to calculate BMI and also listed the application of BMI

--- basicPrograms/16_bodyMassIndex.py
+++ basicPrograms/16_bodyMassIndex.py
@@ -5,7 +5,12 @@
 # status categories, such as underweight, normal weight, overweight, and obesity.
 
 
+# The BMI is calculate using the formula: 
 
+# BMI = weight (kg) / Height (m)^2
+
+# BMI is widely used in public health and clinical settings as a quick and simple tool
+# to assess potential health risks associated with weight.
 
 
 
