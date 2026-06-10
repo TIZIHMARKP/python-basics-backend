b5c84e281e729019dab6db1a3a642bce552c4f5d
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-10 22:47:51 +0100

    Added comments in the code

--- basicPrograms/18_cubeSumOfFirstNaturalNumbers.py
+++ basicPrograms/18_cubeSumOfFirstNaturalNumbers.py
@@ -6,7 +6,8 @@ def cube_sum_of_natural_numbers(n):
     else:
         total = sum([i**3 for i in range(1, n + 1)])
         return total
-    
+
+# Input number of natural numbers
 n = int(input("Enter the value of n: "))
 
 if n <= 0:
