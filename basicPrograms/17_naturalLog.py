65cb42d8083b16b589ef0c2e4f2e7c75426e338e
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-10 22:34:44 +0100

    Added comments to the code explaining what it does

--- basicPrograms/17_naturalLog.py
+++ basicPrograms/17_naturalLog.py
@@ -21,7 +21,6 @@ num = float(input("Enter a number: "))
 if num <= 0:
     print("Please enter a positive number")
 else:
-
+    # calculatx the natural logarithm { base e } of the number
     result = math.log(num)
     print(f"The natural logarithm of {num} is: {result}")
-    
