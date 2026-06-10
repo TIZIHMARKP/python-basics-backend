c6d111e03f9ecd4e7d4482161cda7165973cfce5
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-10 22:26:46 +0100

    Explained what the natural logarithm is about for easy understanding

---
+++ basicPrograms/17_naturalLog.py
@@ -0,0 +1,16 @@
+# Python Program to calculate the natural logarithm of any number.
+
+
+# The natural logarithm, often denoted as , is a mathematical function that represents the
+# logarithm to the base e, where e is the mathematical constant approximately equal to
+# 2.71828. In other words, for a positive number x, the natural logarithm of x is the exponent
+# y that satisfies the equation 
+# e^y = 𝑥
+
+# Mathematically, the natural logarithm is expressed as:
+
+#   ln(x)
+
+# It is commonly used in various branches of mathematics, especially in calculus and
+# mathematical analysis, as well as in fields such as physics, economics, and engineering.
+
