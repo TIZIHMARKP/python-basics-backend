b4f6febe8d892c8655b9b9d407a721a352bb3a96
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-11 23:22:54 +0100

    Explained what an array is in python

---
+++ basicPrograms/19_sumOfArray.py
@@ -0,0 +1,10 @@
+# Python Program to find sum of array.
+
+# In Python, an array is a data structure used to store a collection of elements, each identified
+# by an index or a key. Unlike some other programming languages, Python does not have a
+# built-in array type. Instead, the most commonly used array-like data structure is the list
+
+# A list in Python is a dynamic array, meaning it can change in size during runtime. Elements
+# in a list can be of different data types, and you can perform various operations such as
+# adding, removing, or modifying elements
+
