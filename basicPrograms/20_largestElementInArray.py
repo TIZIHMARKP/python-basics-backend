ebbcc3e48dd77a80b4e41aa22d967aba0aef94e8
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-13 23:54:51 +0100

    Further optimized the code to find largest element in array

--- basicPrograms/20_largestElementInArray.py
+++ basicPrograms/20_largestElementInArray.py
@@ -7,6 +7,7 @@ def find_largest_element(arr):
     # Initializing the first element as the largest
     largest_element = arr[0]
 
+    # Iterate through the array to find the largest element
     for element in arr:
         if element > largest_element:
             largest_element = element
