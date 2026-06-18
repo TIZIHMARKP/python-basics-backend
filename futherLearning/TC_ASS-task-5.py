be91a11b07c9b4b4bfc440a89cfc06fb93899ad6
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-18 17:55:27 +0100

    Successfully writen a python program which defines a set of numbers and finds their intersection & Union

--- futherLearning/TC_ASS-task-5.py
+++ futherLearning/TC_ASS-task-5.py
@@ -3,6 +3,11 @@
 # 3. Write a python program that creates two sets of numbers, finds their intersection and union, and prints the results.
 
 
+set_a = {1, 2, 3}
+set_b = {3, 4, 5}
+
+print("Union: ", set_a.union(set_b))          # Output: {1, 2, 3, 4, 5}
+print("Intersection: ", set_a.intersection(set_b))   # Output: {3}
 
 
 
