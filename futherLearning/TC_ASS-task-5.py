4640140c8b172c48cfd2940999bd8248099f5ec2
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-06-18 18:06:20 +0100

    Writing a python program that defines a nested dictionary representing a classroom and calculating their average score

--- futherLearning/TC_ASS-task-5.py
+++ futherLearning/TC_ASS-task-5.py
@@ -2,12 +2,18 @@
 
 # 3. Write a python program that creates two sets of numbers, finds their intersection and union, and prints the results.
 
+# =================== SOLUTION =========================
+# set_World = {1, 2, 3, 4, 5, 6, 7, 8, 9, }
+# set_Africa = {3, 4, 5, 10}
 
-set_a = {1, 2, 3}
-set_b = {3, 4, 5}
+# print("Union: ", set_World.union(set_Africa))          # Output: {1, 2, 3, 4, 5}
+# print("Intersection: ", set_World.intersection(set_Africa))   # Output: {3}
 
-print("Union: ", set_a.union(set_b))          # Output: {1, 2, 3, 4, 5}
-print("Intersection: ", set_a.intersection(set_b))   # Output: {3}
+
+# 4. Write a program that defines a nested dictionary representing a classroom (e.g., { "class1": {"Alice": 85, "Bob": 90}, "class2": {"Charlie": 78}}). Create a function to calculate the average score for each class and return a dictionary with class names and their averages.
+
+
+
 
 
 
