166ac2d0212bbe819d69b20be0c53267fc2372fb
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-04 03:28:24 +0100

    Added the questions

---
+++ futherLearning/TC_Ass-task-6.py
@@ -0,0 +1,25 @@
+# ==================  QUESTIONS ====================
+
+# 1. What is HTTP, how does it work, and what are the major components of an HTTP response?
+
+# 2. What are the common HTTP methods and their purposes?
+
+# 3. Explain the concept of statelessness in HTTP.
+
+# 4. What is the purpose of HTTP headers?
+
+# 5. Describe the status code categories (1xx, 2xx, 3xx, 4xx, 5xx).
+
+# 6. What is the significance of the 200 and 404 status codes?
+
+# 7. How do query strings work in HTTP?
+
+# 8. What are the basic steps to set up a Flask application?
+
+# 9. Explain the role of the Flask app instance.
+
+# 10. What is the function of the `__name__ == "__main__"` check?
+
+# 11. How can environment variables be used to configure Flask?
+
+
