ae1f60dfd83a52ce0376236016f0006aaa218926
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-08 23:54:00 +0100

    Successfully written a python program to check if array is monotonic

--- basicPrograms/23_is_monotonic.py
+++ basicPrograms/23_is_monotonic.py
@@ -14,3 +14,7 @@ def isMonotonic(arr):
 arr1 = [1, 2, 3, 4]
 arr2 = [3, 2, 1]
 arr3 = [1, 3, 2, 4]
+
+print("Array 1 is monotonic: ", isMonotonic(arr1))
+print("Array 2 is monotonic: ", isMonotonic(arr2))
+print("Array 3 is monotonic: ", isMonotonic(arr3))
