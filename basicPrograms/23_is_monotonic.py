69a57a4fc6a7ea04383bef94a5d35304fc00528c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-08 23:50:28 +0100

    Written the monotonic function

---
+++ basicPrograms/23_is_monotonic.py
@@ -0,0 +1,12 @@
+
+def isMonotonic(arr):
+    increasing = decreasing = True
+
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i - 1]:
+            decreasing = False
+        elif arr[i] < arr[i - 1]:
+            increasing = False
+
+    return increasing or decreasing
+            
