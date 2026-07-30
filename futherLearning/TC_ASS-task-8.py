948c13f3f114173912a85ae302db09af56ff9839
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-07-30 18:23:43 +0100

    Added questions

---
+++ futherLearning/TC_ASS-task-8.py
@@ -0,0 +1,12 @@
+# ========= QUESTIONS =============
+# 1. How does Flask handle form data sent through HTTP requests?
+
+# 2. What is the role of the `request` object in Flask?
+
+# 3. What security concerns are associated with form handling?
+
+# 4. Describe the use of `request.args` and `request.form`.
+
+# 5. Explain the differences between SQL and NoSQL databases.
+
+# ======= ANSWERS ===================
