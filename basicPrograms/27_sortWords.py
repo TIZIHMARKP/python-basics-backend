6f24d87a0b143ba497cc55c5c329a81e061758b3
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-01 22:43:16 +0100

    Added comments

--- basicPrograms/27_sortWords.py
+++ basicPrograms/27_sortWords.py
@@ -4,8 +4,10 @@ my_str = input("Enter a string: ")
 # Breaking down the string into a list of words
 words = [word.capitalize() for word in my_str.split()]
 
+# Sorting the list
 words.sort()
 
+# Displaying the sorted words
 print("The sorted words are: ")
 for word in words:
     print(word)
