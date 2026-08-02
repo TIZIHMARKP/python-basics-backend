e7095a94b77a147fc076f167091a09afbb0f528e
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-02 22:58:15 +0100

    success writing a python program to check if a given number is a disarium Number

---
+++ basicPrograms/28_is_disariumNumber.py
@@ -0,0 +1,26 @@
+
+
+def is_disarium(number):
+    # 
+    num_str = str(number)
+
+    digit_sum = sum(int(i) ** (index + 1) for index, i in enumerate(num_str))
+
+    return digit_sum == number
+
+try:
+    num = int(input("Enter a number: "))
+
+    #
+    if is_disarium(num):
+        print(f"{num} is a Disarium number: ")
+    else:
+        print(f"{num} is not a disarium number: ")
+
+except ValueError:
+    print("Invalid input, please enter a valid number")
+
+
+
+
+
