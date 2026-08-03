05b3f7a71045702b417e6c206138fbcce1af549c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-03 21:29:55 +0100

    Given an example of a Happy Number

--- basicPrograms/30_is_happyNumber.py
+++ basicPrograms/30_is_happyNumber.py
@@ -2,3 +2,13 @@
 # the process, eventually it reaches 1. If the process never reaches 1 but instead loops
 # endlessly in a cycle, the number is not a Happy Number
 
+# For e.g
+# 19 is a Happy Number coz:
+# 1^2 + 9^2 = 82
+# 8^2 + 2^2 = 68
+# 6^2 + 8^2 = 100
+# 1^2 + 0^2 + 0^2 = 1
+
+# The process reaches 1, so 19 is a Happy Number
+
+
