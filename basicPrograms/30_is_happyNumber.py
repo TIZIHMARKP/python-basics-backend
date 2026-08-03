5fd180d99973f2eecebc3e041991a4ec0ba1c208
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-03 21:25:55 +0100

    Explained what a happy number is

---
+++ basicPrograms/30_is_happyNumber.py
@@ -0,0 +1,4 @@
+# Happy Number is a +ve integer that, when you repeatedly replace the number by the sum of the squares of its digits and continue
+# the process, eventually it reaches 1. If the process never reaches 1 but instead loops
+# endlessly in a cycle, the number is not a Happy Number
+
