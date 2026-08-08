b0747be3a39a7818f3053a562446dcb3de29dc48
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-08 23:32:02 +0100

    Added comments

--- basicPrograms/30_is_happyNumber.py
+++ basicPrograms/30_is_happyNumber.py
@@ -20,7 +20,7 @@ def is_happy_number(num):
 
     return num == 1
 
-# 
+# Testing the function with a number
 num = int(input("Enter a number: "))
 if is_happy_number(num):
     print(f"{num} is a Happy Number")
