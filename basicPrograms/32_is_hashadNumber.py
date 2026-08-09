62af543ce7028a4cdc1b77f431a4ac72d2917e71
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-09 21:15:41 +0100

    Added comments

--- basicPrograms/32_is_hashadNumber.py
+++ basicPrograms/32_is_hashadNumber.py
@@ -6,9 +6,9 @@
 # 42 is not a Harshad number because 4 + 2 = 6, and 42 is not divisible by 6.
 
 def is_harshad_number(num):
-
+    # Calculating the sum of the digits of the number
     digit_sum = sum(int(i) for i in str(num))
-
+    # Checking if the number is divisible by the sum of its digits
     return num % digit_sum == 0
 
 num = int(input("Enter a number: "))
