c47ffc3a430829e92e966e2f345ee0e7c334c24b
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-09 21:06:51 +0100

    Given an example of a Harshad number

--- basicPrograms/32_is_hashadNumber.py
+++ basicPrograms/32_is_hashadNumber.py
@@ -1,3 +1,7 @@
 # A Harshad number (or Niven number) is an integer that is divisible by the sum of its digits 
 # In other words, a number is considered a Harshad number if it can be evenly divided by te summ of its own digits
 
+# For example
+# 18 is a Harshad number because 1 + 8 = 9, and 18 is divisible by 9
+# 42 is not a Harshad number because 4 + 2 = 6, and 42 is not divisible by 6.
+
