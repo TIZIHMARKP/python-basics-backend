04bc7e3523184a6cffd7ff6894178a6d8d337133
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-09 21:04:42 +0100

    Explained what a Harshad number is

---
+++ basicPrograms/32_is_hashadNumber.py
@@ -0,0 +1,3 @@
+# A Harshad number (or Niven number) is an integer that is divisible by the sum of its digits 
+# In other words, a number is considered a Harshad number if it can be evenly divided by te summ of its own digits
+
