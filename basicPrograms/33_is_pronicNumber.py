89e264cf0d7f159e07d55d80b654b93243868c97
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-11 22:29:25 +0100

    A little progres printing pronic numbers

--- basicPrograms/33_is_pronicNumber.py
+++ basicPrograms/33_is_pronicNumber.py
@@ -17,4 +17,9 @@ def is_pronic_number(num):
             return True
         return False
 
-    
+print("Pronic numbers between 1 and 100 are: ")
+for i in range(1, 101):
+    if is_pronic_number(i):
+        print(i, end=" | ")
+
+        
