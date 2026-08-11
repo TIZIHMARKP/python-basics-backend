661160f2e66e18615782bfe61b9eeae1299d4991
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-11 22:34:49 +0100

    Added the questions of task 10

---
+++ futherLearning/TC_ASS-task-10.py
@@ -0,0 +1,15 @@
+# =============== TASK 10 QUESTIONS ================
+
+# 1. How do you send requests to an external API in Python and what Python module is used to make HTTP requests?
+
+# 2. What are some common challenges when working with external APIs?
+
+# 3. What is the purpose of Blueprints in Flask?
+
+# 4. What is middleware in web development and how can middleware be implemented in Flask?
+
+# 5. What are Flask hooks and what are they used for? What is the difference between `before_request` and `after_request` hooks?
+
+# 6. Define Authentication and Authorization in the context of web applications. What security practices should be followed when implementing authentication?
+
+
