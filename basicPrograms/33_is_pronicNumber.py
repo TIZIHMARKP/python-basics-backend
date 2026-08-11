388846e75e3b0bd808d2fe447b291abc8603e474
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-11 22:18:28 +0100

    Explained what a pronic number is

---
+++ basicPrograms/33_is_pronicNumber.py
@@ -0,0 +1,6 @@
+# A pronic number, also known as ablong number or rectangular number is a type of
+# figurate number that represents a rectangle. It is the product of two consecutive integers, n
+# and (n + 1). Mathematically, a pronic number can be expressed as: 
+
+# 𝑃𝑛 = 𝑛 ∗ (𝑛 + 1)
+
