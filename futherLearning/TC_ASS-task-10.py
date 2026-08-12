cef0ad314d9eeebbf33e39c6ac1a5cd933da97b4
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-12 07:31:57 +0100

    Done with question 1

--- futherLearning/TC_ASS-task-10.py
+++ futherLearning/TC_ASS-task-10.py
@@ -13,3 +13,12 @@
 # 6. Define Authentication and Authorization in the context of web applications. What security practices should be followed when implementing authentication?
 
 
+# ================= ANSWERS ===================
+
+# Q1. In python, we mostly send requests to an external API by making use of the requests library which provides a simple interface for making HTTP requests and handling responses. 
+# - The process usually involves making a HTTP call (either GET, POST, PUT, or DELETE), passing any required parameters, headers or auth tokens and handling the server's response
+# - To fetch data from an external API, we use the requests.get(). To pass a query parameter, we make use of the params dictionary and to pass a headers (like API keys), we make use of  headers
+# -   To send data to an external API, we use requests.post()
+# - The primary python module used to make HTTP requests is the requests library, which is a third party package that requires installation (pip install requests). It handles HTTP interactions, manages sessions and allows for JSON parsing
+
+
