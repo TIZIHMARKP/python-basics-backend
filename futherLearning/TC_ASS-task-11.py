ffff98cf4dd1f66aa7d23f4260ca68d48d7d5f2c
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-13 12:26:43 +0100

    Added questions

---
+++ futherLearning/TC_ASS-task-11.py
@@ -0,0 +1,15 @@
+# ================= QUESTIONS =========================
+
+# 1. What is the importance of testing in software development? Describe the difference between unit testing and integration testing.
+
+# 2. What is the purpose of debugging? How do you use `print()` for simple debugging in Python? What is `pytest` and how is it used?
+
+# 3. How do you test Flask routes?
+
+# 4. What is the role of environment variables during deployment?
+
+# 5. Todo List API with Basic Authentication
+
+
+
+    
