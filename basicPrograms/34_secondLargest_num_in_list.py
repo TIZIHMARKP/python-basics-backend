dd440d49926be82a2d15730ba559b6ca04b5e799
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-14 23:04:24 +0100

    Success finding second largest number in a list

---
+++ basicPrograms/34_secondLargest_num_in_list.py
@@ -0,0 +1,12 @@
+
+numbers = [30, 10, 45, 5, 20, 31]
+
+numbers.sort(reverse=True)
+
+if len(numbers) >= 2:
+    second_largest = numbers[1]
+    print("The 2nd largest number in the list is: ", second_largest)
+
+else:
+    print("The list does not contain a second largest number")
+
