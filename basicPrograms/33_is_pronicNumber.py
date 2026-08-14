86baa03a3c9d200d538ca18fdfca105ebf2a19d6
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-14 22:52:37 +0100

    Success: printed pronic numbers

--- basicPrograms/33_is_pronicNumber.py
+++ basicPrograms/33_is_pronicNumber.py
@@ -12,14 +12,17 @@
 # 𝑃4 = 4 ∗ (4 + 1) = 20
 
 def is_pronic_number(num):
-    for n in range(1, int(num**0.5) + 1):
-        if n + (n + 1) == num:
+    for n in range(1, num + 1):
+        if n * (n + 1) == num:
             return True
-        return False
+
+        if n * (n + 1) > num:
+            break
+
+    return False
 
 print("Pronic numbers between 1 and 100 are: ")
 for i in range(1, 101):
     if is_pronic_number(i):
         print(i, end=" | ")
 
-# Will continue later
