3f937f05bf3c061344934ea4ed032fe3ce39a4f7
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-14 23:06:30 +0100

    Added comments

--- basicPrograms/34_secondLargest_num_in_list.py
+++ basicPrograms/34_secondLargest_num_in_list.py
@@ -3,7 +3,7 @@ numbers = [30, 10, 45, 5, 20, 31]
 
 numbers.sort(reverse=True) # Sorting the list in descending order
  
-if len(numbers) >= 2:
+if len(numbers) >= 2:     # checking if there are atleast 2 numbers in the list
     second_largest = numbers[1]
     print("The 2nd largest number in the list is: ", second_largest)
 
