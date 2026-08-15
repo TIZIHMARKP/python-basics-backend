d28a7daacaf91e9bbc22dc750ed0fe2c9e9dcdd6
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-15 22:29:14 +0100

    success sorting larges numbers

--- basicPrograms/35_find_n_largest_elements_in_list.py
+++ basicPrograms/35_find_n_largest_elements_in_list.py
@@ -10,3 +10,11 @@ def find_n_largest_elements(lst, n):
 
 numbers = [30, 10, 45, 5, 20, 15, 3, 345, 67, 83, 100, 173, 84, 95]
 
+N = int(input("Input length N = "))
+
+#
+result = find_n_largest_elements(numbers, N)
+
+# 
+print(f"The {N} largest elements in the list are: ", result)
+
