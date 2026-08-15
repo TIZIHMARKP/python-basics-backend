5cf2ba049633bfb41ff24ddca80e9317dbdf3c9a
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-15 22:30:24 +0100

    Added comments

--- basicPrograms/35_find_n_largest_elements_in_list.py
+++ basicPrograms/35_find_n_largest_elements_in_list.py
@@ -1,8 +1,8 @@
 def find_n_largest_elements(lst, n):
-    #
+    # Sorting the list in descending order
     sorted_lst = sorted(lst, reverse=True)
 
-    # 
+    # Getting the first N elements base on users input
     largest_elements = sorted_lst[:n]
 
     return largest_elements
