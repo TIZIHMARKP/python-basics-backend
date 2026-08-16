c90029392ef6e5b1fb63e9f3680a43494f11cee8
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-16 23:08:14 +0100

    Added comments

--- basicPrograms/36_evenNumbers_in_a_list.py
+++ basicPrograms/36_evenNumbers_in_a_list.py
@@ -1,6 +1,7 @@
-
+# sample list of numbers
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 77, 78, 83, 56]
 
+# Using a list comprehension to filter even numbers
 even_numbers = [num for num in numbers if num % 2 == 0]
 
 print("Even numbers in the list are: ", even_numbers)
