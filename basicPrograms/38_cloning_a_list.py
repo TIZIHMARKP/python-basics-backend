3062f4adbb8080aa04313580ced822fe40645a9f
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-17 15:45:07 +0100

    Cloned a list using list() constructor

--- basicPrograms/38_cloning_a_list.py
+++ basicPrograms/38_cloning_a_list.py
@@ -1,5 +1,8 @@
 # 1. Using the slice operator
-original_list = [1, 2, 3, 4, 5]
-cloned_list = original_list[:]
-print("Cloned List: ", cloned_list)
+# original_list = [1, 2, 3, 4, 5]
+# cloned_list = original_list[:]
+# print("Cloned List: ", cloned_list)
 
+original_list = [1, 2, 3, 4, 5]
+cloned_list = list(original_list)
+print("Cloned List", cloned_list)
