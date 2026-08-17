2e38b5f61df10010c76451750df19773da61e042
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-17 15:45:40 +0100

    Added comments

--- basicPrograms/38_cloning_a_list.py
+++ basicPrograms/38_cloning_a_list.py
@@ -3,6 +3,7 @@
 # cloned_list = original_list[:]
 # print("Cloned List: ", cloned_list)
 
+# 2. Using the list() constructor
 original_list = [1, 2, 3, 4, 5]
 cloned_list = list(original_list)
 print("Cloned List", cloned_list)
