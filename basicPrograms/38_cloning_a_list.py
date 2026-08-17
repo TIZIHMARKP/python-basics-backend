dbcdb2cd3ca01ce10ea113074006ccf9914202d5
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-17 15:49:02 +0100

    Cloned a list usingList comprehension

--- basicPrograms/38_cloning_a_list.py
+++ basicPrograms/38_cloning_a_list.py
@@ -4,6 +4,11 @@
 # print("Cloned List: ", cloned_list)
 
 # 2. Using the list() constructor
+# original_list = [1, 2, 3, 4, 5]
+# cloned_list = list(original_list)
+# print("Cloned List", cloned_list)
+
+
 original_list = [1, 2, 3, 4, 5]
-cloned_list = list(original_list)
-print("Cloned List", cloned_list)
+cloned_list = [item for item in original_list]
+print(cloned_list)
