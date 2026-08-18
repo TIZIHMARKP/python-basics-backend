a99916b6ef95a0ef5e85949a91f9a2da6c2e8df0
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-18 17:01:15 +0100

    Writen function to find words

--- basicPrograms/38_find_words.py
+++ basicPrograms/38_find_words.py
@@ -1,2 +1,17 @@
 # Write a Python program to find words which are greater than given length k.
 
+def find_words(words, k):
+    #
+    result = []
+
+    for i in words:
+        #
+        if len(i) > k:
+            #
+            result.append(i)
+
+    return result
+
+
+    
+
