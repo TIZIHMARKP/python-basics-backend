3b2a07cffa2e488c812bda96d0de085ccc1edd9d
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-18 16:55:59 +0100

    Added comment

--- basicPrograms/39_count_occurrences.py
+++ basicPrograms/39_count_occurrences.py
@@ -3,7 +3,7 @@ def count_occurrences(lis, element):
     count = lis.count(element)
     return count
 
-# 
+# Example list
 my_list = [1, 2, 3, 4, 2, 5, 2, 3, 4, 6, 5]
 print("Elements in list; ", my_list)
 element_to_count = int(input("Enter element to count: "))
