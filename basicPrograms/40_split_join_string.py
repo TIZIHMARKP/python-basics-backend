3b657dcc319f9a16ed4fef6be9056ba9bffe04ae
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-19 23:58:11 +0100

    Added comments

--- basicPrograms/40_split_join_string.py
+++ basicPrograms/40_split_join_string.py
@@ -1,7 +1,8 @@
-#
+# Spliting a string into a list of words
 input_str = "Python program to split and join a string"
-word_list = input_str.split()
+word_list = input_str.split()  # By default splting on whitespace
 
+# 
 
 
 
