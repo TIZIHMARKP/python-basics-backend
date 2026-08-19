31c5156ec53b250af2fb634e0b9ee0daf64ff82a
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-08-20 00:02:31 +0100

    Success spliting a string into a list of words

--- basicPrograms/40_split_join_string.py
+++ basicPrograms/40_split_join_string.py
@@ -6,5 +6,10 @@ word_list = input_str.split()  # By default splting on whitespace
 separator = " "   # specifying  the separator between words
 output_str = separator.join(word_list)
 
+# printing the results
+print("Original String: ", input_str)
+print("List of split Words: ", word_list)
+print("Joined String: ", output_str)
+
 
 
