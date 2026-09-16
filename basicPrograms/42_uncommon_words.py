0f23677c4dae71e9f465c8c4be202f3621514247
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-09-16 23:39:47 +0100

    Update: Added comments for explanation

--- basicPrograms/42_uncommon_words.py
+++ basicPrograms/42_uncommon_words.py
@@ -1,11 +1,13 @@
 
 def uncommon_words(str1, str2):
-
+    # Spliting the stings into words, then we convert them to sets
     words1 = set(str1.split())
     words2 = set(str2.split())
 
+    # We find uncommon words by making us of the set difference
     uncommon_words_set = words1.symmetric_difference(words2)
 
+    # We convert the list of uncommon words back to a list
     uncommon_words_list = list(uncommon_words_set)
 
     return uncommon_words_list
@@ -16,4 +18,4 @@ string2 = "Python is a computer language"
 uncommon = uncommon_words(string1, string2)
 
 print("Uncommon words: ", uncommon)
-# uncommon words: ['computer', 'programming']
+# Uncommon words: ['computer', 'programming']
