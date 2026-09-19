9bd7a5af8b165f7292fbf9fb4d486a9d08703f9e
TIZIHMARKP <tizihmarkprincewill@gmail.com>
2026-09-19 23:21:53 +0100

    Added explanations

--- basicPrograms/43_find_duplicates.py
+++ basicPrograms/43_find_duplicates.py
@@ -1,17 +1,18 @@
 
 def find_duplicates(input_str):
-
+    # creating an empty dictionary to store character counts
     char_count = {}
-
+    # Initializing a list to store duplicate chars
     duplicates = []
-
+    # Iterating through each character in the input string
     for i in input_str:
-
+        # If char already in dict, we increment its count
         if i in char_count:
             char_count[i] += 1
         else:
             char_count[i] = 1
 
+    # we iterate through the dict & add characters with count > 1
     for i, count in char_count.items():
         if count > 1:
             duplicates.append(i)
